714d309e5b0d0abb2fb49b122ce5805de218c563
Vikingo <Vikingo@DESKTOP-32F1ASV>
2025-03-02 19:15:01 -0500

    Agregar archivos de proyecto.

---
+++ BotonEscurridizo/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace BotonEscurridizo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEscurridizo = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEscurridizo
+            // 
+            this.btnEscurridizo.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnEscurridizo.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEscurridizo.Location = new System.Drawing.Point(178, 296);
+            this.btnEscurridizo.Name = "btnEscurridizo";
+            this.btnEscurridizo.Size = new System.Drawing.Size(132, 80);
+            this.btnEscurridizo.TabIndex = 0;
+            this.btnEscurridizo.Text = "ACEPTAR";
+            this.btnEscurridizo.UseVisualStyleBackColor = false;
+            this.btnEscurridizo.Click += new System.EventHandler(this.btnEscurridizo_Click);
+            this.btnEscurridizo.MouseEnter += new System.EventHandler(this.btnEscurridizo_MouseEnter);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnCerrar.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrar.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btnCerrar.Location = new System.Drawing.Point(454, 296);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(132, 80);
+            this.btnCerrar.TabIndex = 1;
+            this.btnCerrar.Text = "CERRAR";
+            this.btnCerrar.UseVisualStyleBackColor = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(616, 50);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Desea formatear su dispositivo?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnEscurridizo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEscurridizo;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BotonEscurridizo/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BotonEscurridizo
+{
+    public partial class Form1: Form
+    {
+        private Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            btnEscurridizo.MouseEnter += btnEscurridizo_MouseEnter;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEscurridizo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Desea formatear su dispositivo","", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+        }
+
+        private void btnEscurridizo_MouseEnter(object sender, EventArgs e)
+        {
+            MoverBoton();
+        }
+
+        private void MoverBoton()
+        {
+            int maxX = this.ClientSize.Width - btnEscurridizo.Width;
+            int maxY = this.ClientSize.Height - btnEscurridizo.Height;
+
+            int randomX = rand.Next(0, maxX);
+            int randomY = rand.Next(0, maxY);
+
+            btnEscurridizo.Location = new Point(randomX, randomY);
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado = MessageBox.Show("¿Estás seguro de que deseas cerrar la aplicación?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (resultado == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
